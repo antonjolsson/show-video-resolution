de47469998e526d4fefd2068dee870c0c062a938
Anton J Olsson <anton.j.olsson@bredband.net>
2024-07-18 00:00:00 +0200

    feat: 🎸 show framerate

--- show-video-resolution.js
+++ show-video-resolution.js
@@ -1,30 +1,41 @@
 browser.runtime.onMessage.addListener(notify);
 
 let updateInterval
+let numVideoFrames = 0
 
-function getStats(video, videoWidth, videoHeight) {
-    return video ? `Resolution: <b>${videoWidth} x ${videoHeight}</b>` : 'Could not get video element';
+function getStats(video) {
+    if (!video) {
+        return 'Could not get video element'
+    }
+
+    const videoWidth = video?.videoWidth
+    const videoHeight = video?.videoHeight
+    const quality = video.getVideoPlaybackQuality()
+    const deltaFrames = numVideoFrames > 0 ? quality.totalVideoFrames - numVideoFrames : 'N/A'
+    numVideoFrames = quality.totalVideoFrames
+    return `Resolution: <b>${videoWidth} x ${videoHeight}</b><br>
+            Framerate: <b>${deltaFrames} FPS</b>
+`
 }
 
 function notify()  {
     let result = document.getElementById('video-res-div')
     if (result) {
         result.remove()
+        numVideoFrames = 0
         clearInterval(updateInterval)
         return
     }
 
     const video= document.querySelector('video')
 
-    let videoWidth
-    let videoHeight
-
     const div = document.createElement('div')
     div.id = 'video-res-div'
     div.style.position = 'absolute'
     div.style.display = 'flex'
+    div.style.flexDirection = 'column'
     div.style.justifyContent = 'center'
-    div.style.alignItems = 'center'
+    div.style.alignItems = 'flex-start'
     div.style.top = '1rem'
     div.style.left = '1rem'
     div.style.zIndex = video.style.zIndex + 1
@@ -32,7 +43,7 @@ function notify()  {
     div.style.color = 'white'
     div.style.padding = '1.2rem'
     div.style.borderRadius = '0.5rem'
-    div.style.fontSize = '0.8rem'
+    div.style.fontSize = 'calc(max(0.8rem, 12.8px))'
 
     const closeCross = document.createElement('div')
     closeCross.innerText = 'x'
@@ -43,7 +54,7 @@ function notify()  {
     closeCross.style.cursor = 'pointer'
 
     const p = document.createElement('p')
-    p.innerHTML = getStats(video, video.videoWidth, video.videoHeight)
+    p.innerHTML = getStats(video)
 
     const videoParent = video.parentElement
     videoParent.appendChild(div)
@@ -51,8 +62,6 @@ function notify()  {
     div.appendChild(p)
 
     updateInterval = setInterval(() => {
-        videoWidth = video?.videoWidth
-        videoHeight = video?.videoHeight
-        p.innerHTML = getStats(video, videoWidth, videoHeight)
+        p.innerHTML = getStats(video)
     }, 1000)
 }
